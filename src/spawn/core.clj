8bb281ec44d7097d3358480546ecceedf8eae430
Pat Thomas <pthomas@relaynetwork.com>
2015-04-24 16:32:02 -0400

    More stream operations.

--- src/spawn/core.clj
+++ src/spawn/core.clj
@@ -29,13 +29,42 @@
   `(def ~stream-name ~(make-stream handler-fn initial-state)))
 
 (defn consume-stream-sync
-  [stream len]
-  (for [_ (range len)]
-    (<!! stream)))
+  ([stream]
+   (for [_ (range)]
+     (<!! stream)))
+  ([stream len]
+   (for [_ (range len)]
+     (<!! stream))))
+
+(defn consume-stream-async
+  [stream len handler-fn]
+  (dotimes [_ len]
+    (go
+      (handler-fn (<! stream))))
+  :done)
 
 (comment
+
+  (->> (make-stream (partial * 2) 1)
+       consume-stream-sync
+       (take 20))
+  
+  (-> (make-stream (partial * 2) 10)
+      (consume-stream-sync 10))
+
+  
+  (with-out-str
+    (time
+     (consume-stream-sync (make-stream inc 10) 10)))
+  (<!! (make-stream inc 1))
+  (consume-stream-async (make-stream inc 1) 100 println)
   (defstream adder inc 1)
   (consume-stream-sync (make-stream inc 10) 10)
-  (let [adder (make-stream inc 1)]
-    (<!! (compose-stream #(* % 3) adder)))
+  (<!! (make-stream inc 1))
+  (->> 1
+       (make-stream inc)
+       (compose-stream (partial * 3))
+       (compose-stream #(- % 2))
+       <!!)
+  
   )
