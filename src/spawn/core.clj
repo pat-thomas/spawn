35927a910bbfe6f817475d14cfc03a188aa03084
Pat Thomas <pthomas@relaynetwork.com>
2015-04-24 11:37:15 -0400

    Basic stream operations.

--- src/spawn/core.clj
+++ src/spawn/core.clj
@@ -1,6 +1,28 @@
-(ns spawn.core)
+(ns spawn.core
+  (:require [clojure.core.async :as a :refer [chan go go-loop <! >! <!! >!!]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-stream
+  [handler-fn initial-state]
+  (let [handler-fn (if (symbol? handler-fn)
+                     (ns-resolve *ns* handler-fn)
+                     handler-fn)
+        ch         (chan 1)]
+    (go-loop [state initial-state]
+      (>! ch state)
+      (recur (handler-fn state)))
+    ch))
+
+(defmacro defstream
+  "Returns a channel that is infinitely populated."
+  [stream-name handler-fn initial-state]
+  `(def ~stream-name ~(make-stream handler-fn initial-state)))
+
+(defn consume-stream-sync
+  [stream len]
+  (for [_ (range len)]
+    (<!! stream)))
+
+(comment
+  (defstream adder inc 1)
+  (count (consume-stream adder 10))
+  )
