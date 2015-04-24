2eb7c4e48522dda17ba3d4ffbc4a8efd8f9393fe
Pat Thomas <pthomas@relaynetwork.com>
2015-04-24 11:47:31 -0400

    compose-stream implementation.

--- src/spawn/core.clj
+++ src/spawn/core.clj
@@ -12,6 +12,17 @@
       (recur (handler-fn state)))
     ch))
 
+(defn compose-stream
+  [handler-fn input-stream]
+  (let [handler-fn (if (symbol? handler-fn)
+                     (ns-resolve *ns* handler-fn)
+                     handler-fn)
+        ch         (chan 1)]
+    (go
+      (while true
+        (>! ch (handler-fn (<! input-stream)))))
+    ch))
+
 (defmacro defstream
   "Returns a channel that is infinitely populated."
   [stream-name handler-fn initial-state]
@@ -24,5 +35,7 @@
 
 (comment
   (defstream adder inc 1)
-  (count (consume-stream adder 10))
+  (consume-stream-sync (make-stream inc 10) 10)
+  (let [adder (make-stream inc 1)]
+    (<!! (compose-stream #(* % 3) adder)))
   )
