a78eceb99ec85751b42ab18470989b1b2f6647e4
Pat Thomas <patthomassoftware@gmail.com>
2015-04-24 20:46:47 -0400

    Move streams into own namespace.

--- src/spawn/core.clj
+++ src/spawn/core.clj
@@ -1,41 +1,4 @@
 (ns spawn.core
-  (:require [clojure.core.async :as a :refer [chan go go-loop <! >! <!! >!!]]))
+  (:require [spawn.streams :as streams]))
 
-(defn make-stream
-  [handler-fn initial-state]
-  (let [ch (chan 1)]
-    (go-loop [state initial-state]
-      (>! ch state)
-      (recur (handler-fn state)))
-    ch))
 
-(defn compose-stream
-  [handler-fn input-stream]
-  (let [handler-fn (if (symbol? handler-fn)
-                     (ns-resolve *ns* handler-fn)
-                     handler-fn)
-        ch         (chan 1)]
-    (go
-      (while true
-        (>! ch (handler-fn (<! input-stream)))))
-    ch))
-
-(defmacro defstream
-  "Returns a channel that is infinitely populated."
-  [stream-name handler-fn initial-state]
-  `(def ~stream-name ~(make-stream (eval handler-fn) initial-state)))
-
-(defn consume-stream-sync
-  ([stream]
-   (for [_ (range)]
-     (<!! stream)))
-  ([stream len]
-   (for [_ (range len)]
-     (<!! stream))))
-
-(defn consume-stream-async
-  [stream len handler-fn]
-  (dotimes [_ len]
-    (go
-      (handler-fn (<! stream))))
-  :done)
---
+++ src/spawn/streams.clj
@@ -0,0 +1,41 @@
+(ns spawn.streams
+  (:require [clojure.core.async :as a :refer [chan go go-loop <! >! <!! >!!]]))
+
+(defn make-stream
+  [handler-fn initial-state]
+  (let [ch (chan 1)]
+    (go-loop [state initial-state]
+      (>! ch state)
+      (recur (handler-fn state)))
+    ch))
+
+(defn compose-stream
+  [handler-fn input-stream]
+  (let [handler-fn (if (symbol? handler-fn)
+                     (ns-resolve *ns* handler-fn)
+                     handler-fn)
+        ch         (chan 1)]
+    (go
+      (while true
+        (>! ch (handler-fn (<! input-stream)))))
+    ch))
+
+(defmacro defstream
+  "Returns a channel that is infinitely populated."
+  [stream-name handler-fn initial-state]
+  `(def ~stream-name ~(make-stream (eval handler-fn) initial-state)))
+
+(defn consume-stream-sync
+  ([stream]
+   (for [_ (range)]
+     (<!! stream)))
+  ([stream len]
+   (for [_ (range len)]
+     (<!! stream))))
+
+(defn consume-stream-async
+  [stream len handler-fn]
+  (dotimes [_ len]
+    (go
+      (handler-fn (<! stream))))
+  :done)
